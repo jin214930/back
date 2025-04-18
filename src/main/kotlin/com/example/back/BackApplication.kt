39553b8fe9e8e0ab4904131f2b5fe60a40d76fd6
jin214930 <jin214930@naver.com>
2025-04-18 09:00:15 +0900

    work

--- src/main/kotlin/com/example/back/BackApplication.kt
+++ src/main/kotlin/com/example/back/BackApplication.kt
@@ -2,8 +2,10 @@ package com.example.back
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 class BackApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/com/example/back/domain/post/service/PostService.kt
@@ -0,0 +1,18 @@
+package com.example.back.domain.post.service
+
+import com.example.back.domain.post.entity.Post
+import com.example.back.domain.post.repository.PostRepository
+import org.springframework.stereotype.Service
+
+@Service
+class PostService(
+    private val postRepository: PostRepository
+) {
+    fun count() = postRepository.count()
+
+    fun write(title: String, content: String): Post {
+        return postRepository.save(Post(title = title, content = content))
+    }
+
+    fun findAll() = postRepository.findAll()
+}
